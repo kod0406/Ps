cc1ccf1be4de649d5ccfcce5143d20b7187b11b3
kod0406 <147029200+kod0406@users.noreply.github.com>
2024-09-04 22:14:48 +0900

    [Silver IV] Title: 점화식, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/13699. 점화식/점화식.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[36];
+        dp[0] = 1;
+
+        // 카탈란 수 계산
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - 1 - j];
+            }
+        }
+
+        bw.write(String.valueOf(dp[n]));
+        bw.flush();
+    }
+}
