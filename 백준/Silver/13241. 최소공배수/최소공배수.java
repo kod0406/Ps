4545fc8a6a26e557c0fd0617cecac1effa7abb99
kod0406 <147029200+kod0406@users.noreply.github.com>
2024-09-08 22:48:48 +0900

    [Silver V] Title: 최소공배수, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    // 최대공약수(GCD) 함수
+    public static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    // 최소공배수(LCM) 함수
+    public static long lcm(long a, long b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+
+        System.out.println(lcm(A, B));
+    }
+}
