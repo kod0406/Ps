063043c227b19d02b15fd67214b15d13bcf2fa57
kod0406 <147029200+kod0406@users.noreply.github.com>
2024-11-14 23:02:28 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        StringBuilder result = new StringBuilder();
+
+        for (char c : input.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                result.append(Character.toLowerCase(c));
+            } else {
+                result.append(Character.toUpperCase(c));
+            }
+        }
+
+        System.out.println(result.toString());
+    }
+}
