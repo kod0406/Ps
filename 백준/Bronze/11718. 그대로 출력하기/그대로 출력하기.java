77706a20c209c544f4b3209b5c00629ada72beab
kod0406 <147029200+kod0406@users.noreply.github.com>
2024-11-18 23:57:48 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 104 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+
+        try {
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
