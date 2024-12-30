8bbb5c01d2e6d68854783387b6310ec24bc7af59
kod0406 <kod04061@gmail.com>
2024-12-30 12:13:24 +0900

    Commit ps
    simple ps

---
+++ JungOl/ps_501.java
@@ -0,0 +1,5 @@
+package ch01;
+
+public class Main {
+	//오류 뭔데
+}
---
+++ JungOl/ps_502.java
@@ -0,0 +1,12 @@
+package ch02;
+
+import java.io.*;
+
+class ps_502{
+    public static void main(String args[]) throws IOException{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("Programming! It's fun.");
+        bw.flush();
+        //asaasasa
+    }
+}
